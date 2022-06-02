cbc4fe2a4e311c74d3852d831a15ed5b1d3e0321
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-02 15:49:28 -0700

    printed puts string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+int main(void)
+{
+/** Code body. 
+*Return 0 = success
+*/
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return(0);
+
+}
