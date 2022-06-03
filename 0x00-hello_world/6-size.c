b3a3cf045826aea913300d4dec4fc023a3473dba
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-03 02:01:58 -0700

    printed the size of each data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %lu byte(s)\n", (long unsigned int)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (long unsigned int)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (long unsigned int)sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\n", (long unsigned int)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (long unsigned int)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long int)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long int)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long int)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long int)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long int)sizeof(e));
 
 return (0);
 }
