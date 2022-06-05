7b70538ae3a3af0a2bb8a6dde5a995c614ca57c1
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-04 23:46:35 -0700

    printed the english alphabet in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-
-/**
-*main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-char alph(char a, char z);
-
-int main(void)
-{
-char alph = a;
-while (alph <= z)
-{
-putchar(alph);
-alph++;
-}
-
-return (0);
-}
+>
+> /**
+> *main - Entry point
+> *
+> * Return: Always 0 (Success)
+> */
+> char alph(char a, char z);
+>
+> int main(void)
+> {
+> char alph = a;
+> while (alph <= z)
+> {
+>
+0-positive_or_negative.c
+1-last_digit.c
+2-print_alphabet.c
+README.md
+> putchar(alph);
+>
+0-positive_or_negative.c
+1-last_digit.c
+2-print_alphabet.c
+README.md
+> alph++;
+> }
+>
+> return (0);
+> }
