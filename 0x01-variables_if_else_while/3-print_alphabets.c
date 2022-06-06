9f939a1c66a0ca808228e4c930a4a83a64a603ea
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-05 20:32:46 -0700

    printed the english alphabet in upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,23 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alph = 'a';
 	char alph1 = 'A';
-	
 	while (alph <= 'z')
 	{
-	       putchar(alph);
-               ++alph;	       
+		putchar(alph);
+		++alph;
 	}
+
 	while (alph1 <= 'Z')
 	{
 		putchar(alph1);
 		++alph1;
 	}
 	putchar('\n');
-	return (0);
+	return (0);	
 }
