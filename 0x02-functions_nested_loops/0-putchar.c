dc82869d31a8de56fd3185c2c87135eb34e91338
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-07 15:47:49 -0700

    printed _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
-#include "main.h"
-
-int _putchar(char c);
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - prints _putchar
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * main - prints putchar from the header file
+ * Return 0 (Success)
  */
 
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 8; ++i)
-		_putchar(word[i]);
-	_putchar('\n');
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/0-putchar.s
+++ 0x02-functions_nested_loops/0-putchar.s
@@ -1,8 +1,8 @@
 	.file	"0-putchar.c"
 	.text
-	.globl	_putchar
-	.type	_putchar, @function
-_putchar:
+	.globl	main
+	.type	main, @function
+main:
 .LFB0:
 	.cfi_startproc
 	endbr64
@@ -11,63 +11,30 @@ _putchar:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	%edi, %eax
-	movb	%al, -4(%rbp)
-	leaq	-4(%rbp), %rax
-	movl	$1, %edx
-	movq	%rax, %rsi
-	movl	$1, %edi
-	call	write@PLT
-	leave
+	movl	$95, %edi
+	call	putchar@PLT
+	movl	$112, %edi
+	call	putchar@PLT
+	movl	$117, %edi
+	call	putchar@PLT
+	movl	$116, %edi
+	call	putchar@PLT
+	movl	$99, %edi
+	call	putchar@PLT
+	movl	$104, %edi
+	call	putchar@PLT
+	movl	$97, %edi
+	call	putchar@PLT
+	movl	$114, %edi
+	call	putchar@PLT
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
-	.size	_putchar, .-_putchar
-	.globl	main
-	.type	main, @function
-main:
-.LFB1:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movq	%fs:40, %rax
-	movq	%rax, -8(%rbp)
-	xorl	%eax, %eax
-	movabsq	$8241983569429622879, %rax
-	movq	%rax, -16(%rbp)
-	movl	$0, -20(%rbp)
-	jmp	.L4
-.L5:
-	movl	-20(%rbp), %eax
-	cltq
-	movzbl	-16(%rbp,%rax), %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar
-	addl	$1, -20(%rbp)
-.L4:
-	cmpl	$8, -20(%rbp)
-	jle	.L5
-	movl	$10, %edi
-	call	_putchar
-	movl	$0, %eax
-	movq	-8(%rbp), %rdx
-	xorq	%fs:40, %rdx
-	je	.L7
-	call	__stack_chk_fail@PLT
-.L7:
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
