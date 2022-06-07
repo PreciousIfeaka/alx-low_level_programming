b8cec11aac1af57a48fc123de45d3016d8c1b108
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-06 21:34:32 -0700

    listed single digit decimals with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,12 @@ int main(void)
 	for (numb = 0; numb <= 9; ++numb)
 	{
 		putchar((numb % 10) + '0');
-		
 		if (numb == 9)
-			continue;
+		continue;
 
 		putchar(',');
 		putchar(' ');
 	}
-	
 	putchar('\n');
 	return (0);
 }
