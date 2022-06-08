c2159b6ae09fd42830f267ade560f12243edebdb
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-08 16:33:06 -0700

    checked if an input character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if an input character is an alphabet.
+ * @c: An input character.
+ * Return: 1 if the input is an alphabet or 0 if otherwise.
+ */
+
+int _isalpha(int c)
+{
+	int alph = 65;
+	int i;
+
+	if ((alph > 64 && alph < 91) || (alph > 96 && alph < 123))
+	{
+		alph == c;
+		i = 1;
+	}
+	else
+		i = 0;
+	return (i);
+}
