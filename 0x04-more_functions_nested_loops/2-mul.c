1f4c8e3305bdf6b8c39e8ed9d472f0491273e69d
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-09 13:17:16 -0700

    a function that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first input character
+ * @b: second input character
+ * Return: Always mult (Success).
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 checks for uppercase alphabet
 checks if an input is a digit
+multiplies two integers
