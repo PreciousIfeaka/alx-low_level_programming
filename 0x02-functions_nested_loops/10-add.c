1af50047c6e2fe9ee12cd2a21590d8c78f1fcbcd
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-08 22:23:23 -0700

    returned the addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @x: first input integer.
+ * @y: second input integer.
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+{
+	int m = x + y;
+
+	return (m);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ computed the absoluted value of an integer
 prints last digit of a number
 prints each minute of the day
 prints the 9 times table
+adds two integers
