bce124d90ba790ca77c62014934816e7c6f6a915
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-08 20:18:38 -0700

    printed last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of  number.
+ * @x: input variable
+ * Return: Always last digit.
+ */
+
+int print_last_digit(int x)
+{
+	int m;
+
+	m = x % 10;
+	return (m);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 prints the sign of the input integer
 computed the absoluted value of an integer
+prints last digit of a number
