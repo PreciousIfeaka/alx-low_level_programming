5f980df47387207d41d94a50afb72614706bd7b7
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-14 03:50:45 -0700

    printed alternate characters of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - a function that prints the alternate characters of a string
+ * @str: character pointer
+ * Return: Nothing
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i = 0;
 
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
+		if (i % 2 == 0)
+			_putchar(*str);
+		else
+			continue;
+		str++;
 		i++;
 	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
-	}
-	_putchar('\n');
 }
