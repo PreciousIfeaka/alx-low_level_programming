5a5af8ea472dee7cd1e91745140514b834be6626
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-15 08:59:15 -0700

    printed second half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,8 @@
 
 void puts_half(char *str)
 {
-	int n, i;
+	int n;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
@@ -20,6 +21,7 @@ void puts_half(char *str)
 		else
 			n = (i - 1) / 2;
 		_putchar(str[n++]);
+		i++;
 	}
 	_putchar('\n');
 }
