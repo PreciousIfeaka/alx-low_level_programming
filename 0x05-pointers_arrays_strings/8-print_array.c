94d43f3e2a70047890ea21c30d408b21c7c74d74
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-15 17:27:42 -0700

    printed array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,10 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		if (i == n - 1)
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		else
 			printf("%d, ", a[i]);
 		i++;
 	}
+	putchar('\n');
 }
