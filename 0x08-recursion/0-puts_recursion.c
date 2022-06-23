c58237e32674c9baed9892f9d4a980efe4444868
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-22 17:32:37 -0700

    implemented the puts function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints astring
+ * @s: pointer to a string
+ * Return: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		++s;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
