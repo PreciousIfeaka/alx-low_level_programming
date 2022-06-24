16735e2cb6c9de7d92f9fdb103348bab97ad9227
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-24 14:11:54 -0700

    printed the program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,13 @@
  * main - a function that runs the program
  * @argc: argument count variable
  * @argv: argument vector variable
- * Return: 0 if successful
+ * Return: Always 0 if successful
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 
 	return (0);
 }
