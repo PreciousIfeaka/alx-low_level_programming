243d67d7766bd899d52e29ba6eab452c999c462f
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-25 03:50:41 -0700

    added positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,24 +9,24 @@
  * Return: 0 if successful and 1 if failed
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
 	if (argc < 2)
-		printf("0\n");
+		return (0);
 	if (argc >= 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			if (!atoi(argv[i]))
 			{
-				printf("Error\n");
+				printf("%s\n", "Error");
 				return (1);
 			}
 			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
-		return (0);
 	}
+	return (0);
 }
