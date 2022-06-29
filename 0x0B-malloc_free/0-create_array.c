6c6c3345dc50064db3b7f7cc58eb107b7ec92973
PreciousIfeaka <preciousifeaka@gmail.com>
2022-06-29 02:52:45 -0700

    created and initialized an array with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *buffer;
 
-	buffer = malloc(sizeof(char) * size);
+	buffer = (char *)malloc(sizeof(char) * size);
 	if (size <= 0)
 		return (NULL);
 	else if (size > 0)
