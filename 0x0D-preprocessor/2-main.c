8c6a82c429276c4fe67575b037e99aa894967fa2
PreciousIfeaka <preciousifeaka@gmail.com>
2022-07-04 21:51:31 -0700

    printed the name of the file before compilation

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function thay creates macros
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,2 +1,3 @@
 a header file with macro name SIZE
 a file that defines a macro named PI
+print the name of the file it was compiled from
