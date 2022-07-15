0b82f68b5d5e6c3b9084582844aab7b93a793001
PreciousIfeaka <preciousifeaka@gmail.com>
2022-07-14 18:29:57 -0700

    determined the number of elements in a list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t node = 0;
+	unsigned int element = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		node++;
+		element++;
 	}
-	return (node);
+	return (element);
 }
