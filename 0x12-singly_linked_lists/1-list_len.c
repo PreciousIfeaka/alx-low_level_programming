d06879bfec5fd53a9184b3ef830cebeb70251a85
PreciousIfeaka <preciousifeaka@gmail.com>
2022-07-14 18:55:37 -0700

    updated list_len file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
 
 size_t list_len(const list_t *h)
 {
-	unsigned int element = 0;
+	unsigned int nubr = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		element++;
+		nubr++;
 	}
-	return (element);
+	return (nubr);
 }
