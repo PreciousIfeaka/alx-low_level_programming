8fb1b00dddaccbd4dfcba6df1246ebd23e17d141
PreciousIfeaka <preciousifeaka@gmail.com>
2022-07-16 08:11:29 -0700

    updated init file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).owner = owner;
-	(*d).age = age;
-
-	return (d);
+	if (d)
+	{
+		(*d).name = name;
+		(*d).owner = owner;
+		(*d).age = age;
+	}
 }
