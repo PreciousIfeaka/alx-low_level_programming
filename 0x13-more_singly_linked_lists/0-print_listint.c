9311e74de2e7b5436ec0b97b06ad542a726e7a0c
PreciousIfeaka <preciousifeaka@gmail.com>
2022-07-18 04:57:30 -0700

    printed the elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_listint - a function that prints all the elements of a list
+ * @h: a pointer to the head node
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->next == NULL)
+			return (0);
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+prints the elements in a linked list
