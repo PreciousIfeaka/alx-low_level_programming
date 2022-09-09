bc09f6109780a8ecf98a017f2cffc28aa8855b6e
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-08 23:09:33 -0700

    added a node to the end of a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,7 +5,7 @@
  * @head: a pointer to the head pointer
  * @n: the data to be added
  *
- * return: new element address if successful, else NULL
+ * Return: new element address if successful, else NULL
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
