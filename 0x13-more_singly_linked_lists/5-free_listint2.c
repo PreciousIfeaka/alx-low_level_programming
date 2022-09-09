af320b8d4a38c72df34b654b55b823f065e6cb57
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-09 00:46:44 -0700

    freed a list and initialized head to nil

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: a pointer to the head node
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *front, *current;
+
+	current = *head;
+
+	while (current)
+	{
+		front = current->next;
+		free(current);
+		current = front;
+	}
+	*head = NULL;
+}
