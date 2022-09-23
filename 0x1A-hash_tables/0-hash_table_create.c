5e50a6eb66d843a868b474982b40f53fc54e48aa
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-22 17:57:26 -0700

    created a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
-	table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (!table)
 		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!(table->array))
+		return NULL;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 	
 	return (table);
 }
