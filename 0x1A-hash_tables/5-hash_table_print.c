dd980235667e210337595a28e524cfed7a4f54e1
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-24 06:02:31 -0700

    printed the table in python dict style

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the elements in a hash table
+ * @ht: a pointer to the hash table
+ *
+ * Return: nothing(void function)
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i, put_comma = 0;
+
+	if (ht == NULL)
+		return;
+	putchar('{');
+
+	for (i = 0; i <= ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (put_comma == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s' : '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			put_comma = 1;
+		}
+	}
+	printf("}\n");
+}
