8187b3084cc328d90c5e04bcff2d5a04c32d56b9
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-25 11:07:17 -0700

    concatenated two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,11 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i, index1, index2;
+	unsigned long int i, index1, index2;
 
 	if (!s1)
 		s1 = "";
-	else if (!s2)
+	if (!s2)
 		s2 = "";
 
 	i = strlen(s1) + strlen(s2);
@@ -26,13 +26,14 @@ char *str_concat(char *s1, char *s2)
 		free(s);
 		return (NULL);
 	}
-	for (index1 = 0; s1[index1] != '\0'; index1++)
+	for (index1 = 0; index1 < strlen(s1); index1++)
 	{
 		s[index1] = s1[index1];
 	}
-	for (index2 = 0; s2[index2] != '\0'; index2++, index1++)
+	for (index2 = 0; index2 < strlen(s2); index2++, index1++)
 	{
 		s[index1] = s2[index2];
 	}
+	s[index1++] = '\0';
 	return (s);
 }
