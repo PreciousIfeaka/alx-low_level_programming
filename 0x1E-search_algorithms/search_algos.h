2bd7c6fed51fc1038141ce9324be648f85fe4c0f
root <root@vagrant.vm>
2023-07-13 13:37:44 +0000

    pushed header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,5 +2,7 @@
 #define SEARCH_ALGOS_H
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, int L, int R);
+int binary_search(int *array, size_t size, int value);
 
 #endif
